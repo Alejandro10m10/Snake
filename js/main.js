6908f93faa2deeecc7a8712c1c317905a03489ff
Alejandro Sánchez <alejandro10m_568@hotmail.com>
2021-12-23 15:13:06 -0600

    Dibujando las paredes de los limites del juego

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+var canvas = document.querySelector('#juegoCanvas');
+var lienzo = canvas.getContext("2d");
+var widthCanvas = canvas.width;
+var heightCanvas = canvas.height;
+
+dibujarParedes(lienzo, 20, 20, widthCanvas, heightCanvas);
+
+function dibujarParedes(lienzo, xInicial, yInicial, widthCanvas, heightCanvas){
+    xFinalyFinal = xInicial + yInicial;
+    lienzo.beginPath();
+    lienzo.lineWidth = "2";
+    lienzo.rect(xInicial, yInicial, widthCanvas-xFinalyFinal, heightCanvas-xFinalyFinal);
+    lienzo.stroke();
+}
